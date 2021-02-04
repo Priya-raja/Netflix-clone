a7040659be44066b96a3494d0ebe50debcbd82e0
Priya-raja <priya.thevar89@gmail.com>
2021-02-04 20:54:10 +0400

    Added Router inital page

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,12 +1,28 @@
 import React from "react"
+import { BrowserRouter  as Router, Switch } from "react-router-dom";
+import { Route } from  "react-router-dom";
 
-function App() {
+
+export function App() {
 
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  )
+    <Router>
+      <Switch>
+        <Route path="/browse">
+          <p>I will  be the sign in page</p> 
+        </Route>
+        <Route path="/signin">
+          <p>I will be the sign-up page</p>
+        </Route>
 
+        <Route path="/browse">
+          <p>I will be the browse page</p>
+        </Route>
+        <Route path="/">
+          <p>I am going to be a cloned Netflix application</p>
+
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-export default App
