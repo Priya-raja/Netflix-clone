5da1cfe146ea483844333b75a9a6f467851f76e0
Priya-raja <priya.thevar89@gmail.com>
2021-02-08 21:02:10 +0400

    Added home component to pages

--- netflix/src/components/jumbotron/index.js
+++ netflix/src/components/jumbotron/index.js
@@ -10,5 +10,5 @@ export default function Jumbotron({ children, direction = 'row', ...restProps})
    )
 }
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps}) {
-    
+    return <Container {...restProps}>{children}</Container>
 }
